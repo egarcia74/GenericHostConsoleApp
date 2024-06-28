c756fc9efb934d93c5a41c21391484b0234cace4
Eduardo Garcia-Prieto <egarcia74@gmail.com>
2024-06-28 11:30:57 +1000

    Enhance configuration and service setup in GenericHostConsoleApp
    This commit primarily adds detailed configurations and services to the Generic Host Console application. In particular, it sets the content root directory for the host, incorporates user secrets into the configuration, adds the needed services, and enhances configuration for the WeatherOptions type. Also, it bolsters the application's logging capabilities and ensures the Host instance starts as a console app.

--- GenericHostConsoleApp/Program.cs
+++ GenericHostConsoleApp/Program.cs
@@ -9,12 +9,15 @@ using Serilog;
 
 // Configure and start the application host. 
 await Host.CreateDefaultBuilder(args)
+    // Set the content root directory for the host instance.
     .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
                     ?? throw new InvalidOperationException("Failed to get the path to the current assembly."))
     .ConfigureAppConfiguration((_, builder) =>
     {
+        // Add user secrets to the configuration
         builder.AddUserSecrets<Program>(true, true);
 
+        // Uncomment below code to add a command line configuration provider:
         // builder.AddCommandLine(args, new Dictionary<string, string>
         // {
         //     // Define parameter mappings.
@@ -24,19 +27,23 @@ await Host.CreateDefaultBuilder(args)
     })
     .ConfigureServices((hostContext, services) =>
     {
+        // Add the needed services
         services
             .AddHostedService<ApplicationHostedService>()
             .AddTransient<IMainService, MainService>()
             .AddSingleton<IWeatherService, WeatherService>()
             .AddSingleton<IUserNotificationService, UserNotificationService>();
 
+        // Set additional actions to take when configuring the provided WeatherOptions type.
         services
             .AddOptions<WeatherOptions>()
             .Bind(hostContext.Configuration.GetSection(nameof(WeatherOptions)))
             .ValidateDataAnnotations()
             .ValidateOnStart();
     })
-    .UseSerilog((context, configuration) => 
+    // Add logging capabilities
+    .UseSerilog((context, configuration) =>
         configuration
             .ReadFrom.Configuration(context.Configuration))
+    // Start the host instance as a console application.
     .RunConsoleAsync();
