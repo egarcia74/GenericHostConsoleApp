54364c09aeaa5b4b73bb117cfed9ac6f5ebf4269
Eduardo Garcia-Prieto <egarcia74@gmail.com>
2024-08-12 12:54:58 +1000

    Add logging to WeatherForecastService
    Introduce logging with ILogger to WeatherForecastService to log the OpenWeather URL before making the GET request, improving traceability and debugging.

--- GenericHostConsoleApp/Services/WeatherForecastService.cs
+++ GenericHostConsoleApp/Services/WeatherForecastService.cs
@@ -1,11 +1,12 @@
 using GenericHostConsoleApp.Configuration;
 using GenericHostConsoleApp.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace GenericHostConsoleApp.Services;
 
 /// Represents a service for fetching weather forecasts.
-public class WeatherForecastService(HttpClient httpClient, IOptions<WeatherForecastServiceOptions> options)
+public class WeatherForecastService(HttpClient httpClient, IOptions<WeatherForecastServiceOptions> options, ILogger<WeatherForecastService> logger)
     : IWeatherForecastService
 {
     /// <summary>
@@ -17,9 +18,11 @@ public class WeatherForecastService(HttpClient httpClient, IOptions<WeatherForec
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var response =
-            await httpClient.GetAsync($"{options.Value.Url}?q={options.Value.City}&appid={options.Value.ApiKey}",
-                cancellationToken);
+        var url = $"{options.Value.Url}?q={options.Value.City}&appid={options.Value.ApiKey}";
+        
+        logger.LogInformation("OpenWeather Url: {url}", url);
+
+        var response = await httpClient.GetAsync(url, cancellationToken);
 
         if (!response.IsSuccessStatusCode)
             throw new Exception($"Failed to fetch weather data: Status: {response.StatusCode}; {response.Content}");
