f05ef64a1d8e7d6f33eef9d0f085800e9a96ae4e
Eduardo Garcia-Prieto <egarcia74@gmail.com>
2024-08-15 11:37:07 +1000

    Add XML documentation to WeatherForecastServiceOptions
    Added XML comments to provide clear descriptions for each property in WeatherForecastServiceOptions. This improves code readability and aids in understanding the purpose of each configuration setting.
    
    Signed-off-by: Eduardo Garcia-Prieto <egarcia74@gmail.com>

--- GenericHostConsoleApp/Configuration/WeatherForecastServiceOptions.cs
+++ GenericHostConsoleApp/Configuration/WeatherForecastServiceOptions.cs
@@ -2,9 +2,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace GenericHostConsoleApp.Configuration;
 
+/// <summary>
+/// Represents the options for the WeatherForecastService.
+/// </summary>
 public class WeatherForecastServiceOptions
 {
+    /// <summary>
+    /// Represents the API key used for authentication in the WeatherForecastService.
+    /// </summary>
     [Required] public string? ApiKey { get; set; }
+
+    /// <summary>
+    /// Represents the options for the WeatherForecastService.
+    /// </summary>
     [Required] public string? Url { get; set; }
+
+    /// <summary>
+    /// Represents the city used for fetching weather forecasts in the WeatherForecastService.
+    /// </summary>
     [Required] public string? City { get; set; }
 }
